ea5e127690fdadb86e32d9ab4a33d7d5d9803014
DimirZC <121239098+DimirZC@users.noreply.github.com>
2026-07-05 22:36:30 -0300

    Passo-04

---
+++ aula-tdd/src/main/java/com/exemplo/Franc.java
@@ -0,0 +1,11 @@
+package com.exemplo;
+
+class Franc extends Money {
+    Franc(int amount) {
+        this.amount = amount;
+    }
+    
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+}
---
+++ aula-tdd/src/main/java/com/exemplo/Money.java
@@ -0,0 +1,10 @@
+package com.exemplo;
+
+class Money {
+    protected int amount;
+    
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
+}
