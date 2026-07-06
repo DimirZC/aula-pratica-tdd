7522411ac1c1d2fff42fab68605c46ea0f064988
DimirZC <121239098+DimirZC@users.noreply.github.com>
2026-07-05 21:54:26 -0300

    Passo-01

---
+++ aula-tdd/src/main/java/com/exemplo/Dollar.java
@@ -0,0 +1,13 @@
+package com.exemplo;
+
+class Dollar {
+    int amount = 10;
+    
+    Dollar(int amount) {
+        // construtor vazio por enquanto
+    }
+    
+    void times(int multiplier) {
+        // método vazio por enquanto
+    }
+}
